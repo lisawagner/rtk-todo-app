e2edc484476fe65a25b7b36fa858589c51dd0ce8
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-22 12:29:45 -0700

    feat: add app scaffolding

---
+++ src/features/todo/index.js
@@ -0,0 +1,7 @@
+import AddTodoForm from './AddTodoForm'
+import TodoItem from './TodoItem'
+import TodoList from './TodoList'
+import todoSlice from './todoSlice'
+import TotalCompleteItems from './TotalCompleteItems'
+
+export { AddTodoForm, TodoItem, TodoList, todoSlice, TotalCompleteItems }
