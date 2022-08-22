7a92524cd58a85fb2f795504ca2e391a64a2211f
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-22 13:26:58 -0700

    feat: update feature directory index.js

--- src/features/todo/index.js
+++ src/features/todo/index.js
@@ -1,7 +1,5 @@
 import AddTodoForm from './AddTodoForm'
-import TodoItem from './TodoItem'
 import TodoList from './TodoList'
-import todoSlice from './todoSlice'
 import TotalCompleteItems from './TotalCompleteItems'
 
-export { AddTodoForm, TodoItem, TodoList, todoSlice, TotalCompleteItems }
+export { AddTodoForm, TodoList, TotalCompleteItems }
